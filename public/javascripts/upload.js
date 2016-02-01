299c6d42d5e6a1adc1be87305d7128f0fda9f5e8
johnreedlol <johnmichaelreedfas@gmail.com>
2016-02-01 18:13:31 -0500

    Added alert to upload.js
    Can understand what client is doing

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,23 +1,29 @@
+$(function () {
 
-$(function(){
-    var r = new Resumable({
-        target:'/resumable'
-    });
+  var resumable = new Resumable({
+    target: '/resumable'
+  });
 
-    r.assignBrowse(document.getElementById('browseButton'));
-    //r.on('fileAdded', function(file){
-    //    r.upload();
-    //});
-    $("#button").click( function()
-        {
-            r.upload();
-        }
-    );
-    r.on('fileSuccess', function(file){
-        console.debug(file);
-    });
-    r.on('fileProgress', function(file){
-        console.debug(file);
-    });
+  resumable.assignBrowse(document.getElementById('browseButton'));
+
+  //resumable.on('fileAdded', function(file){
+  //    resumable.upload();
+  //});
+
+  $("#button").click(function () {
+      resumable.upload()
+    }
+  );
+
+  resumable.on('fileSuccess', function (file) {
+    console.debug(file);
+    alert('fileSuccess: ' + file); // Appears when done.
+  });
+
+  resumable.on('fileProgress', function (file) {
+    console.debug(file);
+    alert('fileProgress: ' + file); // Corresponds to 'resumableInfo doesn't containsChunk #1. NotFound.'
+    // Also corresponds to completing a post - Upload finished = true. Removed resumableInfo..
+  });
 // more events, look API docs
 });
